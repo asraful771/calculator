e056af4a386aba2623e34b582b6b33166bc0b795
asraful771 <ahsraful.freelancer@gmail.com>
2021-12-21 22:30:13 +0600

    first calculater

---
+++ script.js
@@ -0,0 +1,32 @@
+// for(var i=1; i<=100; i++){
+//     if(i==50){
+//         continue;
+//     }
+//     document.write(" " + i);
+   
+// }
+
+// var i = prompt("Enter naumber");
+// var e = prompt("Enter naumber");
+// if(i%2 == 0 ){
+//     document.write( i + "  is Even number");
+// }else{
+//     document.write(  i +  "  is odd number, ");
+// }
+
+
+// if(i>0 ){
+//     document.write( i + " is positive number");
+// }else{
+//     document.write(  i +  "  is negative number ");
+// }
+//  document.write('<br>');
+// var result = i>0 ? "positive" : " negative";
+// document.write(result);
+
+// function script(){
+//     var result = i*e;
+//     document.write(" result is = " + result );
+// }
+
+// script();
